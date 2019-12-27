40622f98aae42a3ea74da7b2741550f6293f373b
crynomore-d17 <dheaayu044@gmail.com>
2019-12-27 10:25:11 +0700

    tugas pertama python

---
+++ tugas-python1.py
@@ -0,0 +1,12 @@
+A = int(input("Nilai pertama: "))
+B = int(input("Nilai kedua: "))
+C = int(input("Nilai ketiga: "))
+D = A + B + C
+E = A - B - C
+F = A * B * C
+G = A // B // C
+
+print("%d + %d + %d = "%(A,B,C),D)
+print("%d - %d - %d = "%(A,B,C),E)
+print("%d * %d * %d = "%(A,B,C),F)
+print("%d / %d / %d = "%(A,B,C),G)
